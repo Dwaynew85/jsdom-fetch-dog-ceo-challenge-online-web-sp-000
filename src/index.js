5841619961a1842336b78a7bd47e22aef56a9f9e
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-27 18:04:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let dogImgs = [];
+fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => dogImgs = json.message);
+
+function addImages(images){
+  for (const dog of images) {
+      const dogContainer = document.getElementById('dog-image-container');
+      const elem = document.createElement("img");
+      dogContainer.appendChild(elem).src = dog;
+    }
+}
+addImages(dogImgs)
+// for (const dog of dogImgs) {
+//     const dogContainer = document.getElementById('dog-image-container');
+//     const elem = document.createElement("img");
+//     dogContainer.appendChild(elem).src = dog;
+//   }
