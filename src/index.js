c07c7e7cb3fe13ea6116c164004de0fdef570d97
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-27 18:17:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 let dogImgs = [];
 
-function addImages(images){
-  for (const dog of images) {
-      const dogContainer = document.getElementById('dog-image-container');
-      const elem = document.createElement("img");
-      elem.src = dog;
-      dogContainer.appendChild(elem);
-    }
-}
-
-fetch(imgUrl)
-  .then(resp => resp.json())
-  .then(json => dogImgs = json.message);
-
-document.addEventListener('DOMContentLoaded', function() {
-  addImages(dogImgs);
-})
+// function addImages(images){
+//   for (const dog of images) {
+//       const dogContainer = document.getElementById('dog-image-container');
+//       const elem = document.createElement("img");
+//       elem.src = dog;
+//       dogContainer.appendChild(elem);
+//     }
+// }
+// 
+// fetch(imgUrl)
+//   .then(resp => resp.json())
+//   .then(json => dogImgs = json.message);
+// 
+// document.addEventListener('DOMContentLoaded', function() {
+//   addImages(dogImgs);
+// })
